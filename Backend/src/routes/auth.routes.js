d3057b4e50fb57bdb43c41d4208723f6e8b34454
Paban-1 <rpaban89@gmail.com>
2026-09-05 00:23:41 +0530

    re-structure folders

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -2,6 +2,7 @@ const { Router } = require("express")
 const authController = require("../controllers/auth.controller")
 const authMiddleware = require("../middlewares/auth.middleware")
 
+// Auth routes
 const authRouter = Router()
 
 
